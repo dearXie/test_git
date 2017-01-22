a47118c43acf7709e315128815252184e4f26af5
xieq <318711439@qq.com>
2017-01-22 17:04:14 +0800

    test

---
+++ src/main/java/com/asion/xieq/test_git/App.java
@@ -0,0 +1,13 @@
+package com.asion.xieq.test_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
